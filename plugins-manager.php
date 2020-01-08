ffe6cd1665bb1a7be56f573179e3fd8797aad0ec
Sirius Nottin <30043962+siriusnottin@users.noreply.github.com>
2020-01-08 16:48:50 +0100

    Create plugins-manager.php

---
+++ plugins-manager.php
@@ -0,0 +1,17 @@
+<?php
+/*
+Plugin Name:  Plugins Manager
+Plugin URI:   https://github.com/PrimeTimeCode/wp-plugin-activation-manifest
+Author:       Sirius Nottin
+Author URI:   https://nottin.me
+Description:  Manage the activation status of the website's plugins
+Version:      1.0.0
+License:      MIT License
+*/
+/**
+ * More info: https://github.com/PrimeTimeCode/wp-plugin-activation-manifest
+ */
+if ( defined('WP_ENV') ) {
+    require( $root_dir . '/vendor/autoload.php');
+    \PrimeTime\WordPress\PluginManifest\Activation::set( $root_dir . '/plugin-manifest.yml', getenv('WP_ENV'));
+}
